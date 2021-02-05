9cd1d843d0fb0e46d6bbc8fc06ac883c2e9032e8
Eric Tan <erictan1222@gmail.com>
2021-02-05 19:56:33 +0800

    First Commit

---
+++ LABSKILL6.cpp
@@ -0,0 +1,25 @@
+//LABSKILL5&6
+#include<iostream>
+#include<conio.h>
+#include <iomanip>
+using namespace std;
+int main (){
+    double fahrenheit_1,celsius_1=40,fahrenheit_2=120,celsius_2;
+
+    cout<<"Celsius \tFahrenheit \t| \tFahrenheit \tCelsius\n";
+
+ do
+    {
+
+         celsius_2= (fahrenheit_2-32)*5/9;
+        fahrenheit_1= (9.0 / 5) * celsius_1+ 32;
+
+
+        cout<<fixed<<setprecision(1);
+        cout<<celsius_1<<"\t\t"<<fahrenheit_1<<"\t\t|\t"<<fahrenheit_2<<"\t\t"<<celsius_2<<"\t\n";
+        celsius_1--;
+        fahrenheit_2-=10;
+    }
+     while(celsius_1>=31&&fahrenheit_2>=30);
+
+}
